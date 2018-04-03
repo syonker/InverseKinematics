378402476795e30f49a4a0060877cec0df899803
Shea Yonker <sbyonker@gmail.com>
2018-04-02 21:47:17 -0700

    Update DOF.h

--- Project0/DOF.h
+++ Project0/DOF.h
@@ -10,13 +10,9 @@ public:
 	~DOF();
 	void SetValue(float newValue);
 	float GetValue();
-	void SetMinMax(float minValue, float maxValue);
-
 	float Add(float addVal);
 
 	float value;
-	float min;
-	float max;
 
 private:
 
